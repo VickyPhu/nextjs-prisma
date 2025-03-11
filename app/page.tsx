d53ca236dd0c69fc041932a0f315f6d17aad9f8f
VickyPhu <victoriaphu@gmail.com>
2025-03-11 11:53:28 +0100

    delete post action

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { db } from "@/prisma/db";
+import DeletePostButton from "./ui/delete-post-button";
 
 export default async function Home() {
   const posts = await db.post.findMany();
@@ -7,7 +8,12 @@ export default async function Home() {
     <main className="grid gap-8 p-4">
       {posts.map((post) => (
         <article key={post.id} className="grid p-2 rounded hover:bg-slate-800">
-          <span className="text-slate-200">{post.createdAt.toLocaleDateString()}</span>
+          <section className="flex justify-between items-center">
+            <span className="text-slate-200">
+              {post.createdAt.toLocaleDateString()}
+            </span>
+            <DeletePostButton post={post} />
+          </section>
           <h2 className="text-2xl">{post.title}</h2>
           <p className="text-slate-300">{post.content}</p>
         </article>
